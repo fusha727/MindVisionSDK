1ae50cf13790dee1616705225ea5bfc9dd30fafc
fusha727 <68038608+fusha727@users.noreply.github.com>
2020-09-05 10:30:16 +0800

    1visionSDK-回调

---
+++ 1visionSDK-回调/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace _1visionSDK_回调
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(561, 294);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "关闭相机";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(561, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "实时采集2回";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(561, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "实时采集1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(561, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "采图一次";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(561, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "打开相机";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Aqua;
+            this.pictureBox1.Location = new System.Drawing.Point(149, 101);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 249);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "回调采集";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 1visionSDK-回调/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MVSDK;
+using CameraHandle = System.Int32;
+using MvApi = MVSDK.MvApi;
+
+
+namespace _1visionSDK_回调
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        #region variable
+        protected CameraHandle m_hCamera = 0;             // 句柄
+        protected IntPtr m_ImageBuffer;             // 预览通道RGB图像缓存
+        protected IntPtr m_ImageBufferSnapshot;     // 抓拍通道RGB图像缓存
+        protected tSdkCameraCapbility tCameraCapability;  // 相机特性描述
+        protected int m_iDisplayedFrames = 0;    //已经显示的总帧数
+        protected CAMERA_SNAP_PROC m_CaptureCallback;
+        protected IntPtr m_iCaptureCallbackCtx;     //图像回调函数的上下文参数
+        protected tSdkFrameHead m_tFrameHead;
+        //显示抓拍图像的窗口
+        protected bool m_bEraseBk = false;
+        protected bool m_bSaveImage = false;
+        CameraSdkStatus status;     //相机SDK的状态  
+        tSdkCameraDevInfo[] tCameraDevInfoList;
+        CAMERA_SNAP_PROC pCaptureCallOld;
+        #endregion
+
+        //1.枚举设备
+        //2.初始化相机
+        //3.申请内存
+        //4.抓图模式的设置-回调函数、开线程。（copy对应函数）--绑定窗口
+        //5.显示模式（单次采集、实时采集、硬触发）
+        //  软触发：设置按钮，按一次触发一次。（单次采集、实时采集）
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //1.枚举设备
+            status = MvApi.CameraEnumerateDevice(out tCameraDevInfoList);
+
+            //2.初始化设备
+            if (tCameraDevInfoList != null)//此时iCameraCounts返回了实际连接的相机个数。如果大于1，则初始化第一个相机
+            {
+                status = MvApi.CameraInit(ref tCameraDevInfoList[0], -1, -1, ref m_hCamera);
+            }
+            //3.申请内存
+            //MvApi.CameraGetCapability(m_hCamera, out tCameraCapability);
+            //m_ImageBuffer = Marshal.AllocHGlobal(tCameraCapability.sResolutionRange.iWidthMax * tCameraCapability.sResolutionRange.iHeightMax * 3 + 1024);
+            //初始化显示模块，使用SDK内部封装好的显示接口
+            MvApi.CameraDisplayInit(m_hCamera, pictureBox1.Handle);
+            MvApi.CameraSetDisplaySize(m_hCamera, pictureBox1.Width, pictureBox1.Height);
+            //4.  回调函数  #if USE_CALL_BACK
+            m_CaptureCallback = new CAMERA_SNAP_PROC(ImageCaptureCallback);
+            MvApi.CameraSetCallbackFunction(m_hCamera, m_CaptureCallback, m_iCaptureCallbackCtx, ref pCaptureCallOld);
+            //5.显示模式（单次采集、实时采集、硬触发）
+            MvApi.CameraPlay(m_hCamera);
+        }
+
+        public void ImageCaptureCallback(CameraHandle hCamera, IntPtr pFrameBuffer, ref tSdkFrameHead pFrameHead, IntPtr pContext)
+        {
+            MvApi.CameraDisplayRGB24(hCamera, pFrameBuffer, ref pFrameHead);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MvApi.CameraPlay(m_hCamera);
+        }
+
+
+ 
+
+    }
+}
